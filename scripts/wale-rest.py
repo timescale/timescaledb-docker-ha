cf34c671f480fb2a2c3a491095bd9a92407608b9
Rob Kiefer <rob.kiefer@gmail.com>
2017-11-02 11:49:53 -0400

    Fix problems with RECOVERY

--- scripts/wale-rest.py
+++ scripts/wale-rest.py
@@ -11,8 +11,9 @@ from logging.config import dictConfig
 API_PORT = int(os.getenv('WALE_LISTEN_PORT', '5000'))
 WAL_PATH = os.getenv('WAL_PATH', '/data/wal/pgwal/')
 WALE_BIN = os.getenv('WALE_BIN', '/usr/local/bin/wal-e')
-WALE_PUSH_FLAGS = os.getenv('WALE_PUSH_FLAGS', '--terse')
-WALE_FETCH_FLAGS = os.getenv('WALE_FETCH_FLAGS', '')
+WALE_FLAGS = os.getenv('WALE_FLAGS', '--terse')
+WALE_PUSH_FLAGS = os.getenv('WALE_PUSH_FLAGS', '')
+WALE_FETCH_FLAGS = os.getenv('WALE_FETCH_FLAGS', '-p=0')
 
 
 class WaleWrapper:
@@ -41,6 +42,7 @@ class WaleWrapper:
         logging.basicConfig(stream=sys.stdout, level=logging.DEBUG)
 
         # create routes
+        self.api.add_url_rule('/ping', view_func=self.ping, methods=['GET'])
         self.api.add_url_rule('/wal-push/<path:path>', view_func=self.push, methods=['GET'])
         self.api.add_url_rule('/wal-fetch/<path:path>', view_func=self.fetch, methods=['GET'])
 
@@ -69,10 +71,16 @@ class WaleWrapper:
 
         file_path = WAL_PATH + path
 
+        command = [WALE_BIN]
+        if len(WALE_FLAGS) > 0:
+            for s in WALE_FLAGS.split():
+                command.append(s)
+        command.append('wal-push')
         if len(WALE_PUSH_FLAGS) > 0:
-            command = [WALE_BIN, WALE_PUSH_FLAGS, 'wal-push', file_path]
-        else:
-            command = [WALE_BIN, 'wal-push', file_path]
+            for s in WALE_PUSH_FLAGS.split():
+                command.append(s)
+        command.extend([file_path])
+        print(command)
 
         return self.perform_command(command,
                                     'Pushing wal file {}'.format(file_path),
@@ -87,15 +95,26 @@ class WaleWrapper:
         file_id = path
 
         file_path  = WAL_PATH + '/' + file_id
-        if WALE_FETCH_FLAGS != '':
-            command = [WALE_BIN, WALE_FETCH_FLAGS, 'wal-fetch', file_id, file_path]
-        else:
-            command = [WALE_BIN, 'wal-fetch', file_id, file_path]
+
+        command = [WALE_BIN]
+        if len(WALE_FLAGS) > 0:
+            for s in WALE_FLAGS.split():
+                command.append(s)
+        command.append('wal-fetch')
+        if len(WALE_FETCH_FLAGS) > 0:
+            for s in WALE_FETCH_FLAGS.split():
+                command.append(s)
+        command.extend([file_id, file_path])
+        print(command)
 
         return self.perform_command(command,
                                     'Fetching wal {}'.format(file_id),
                                     'Failed to fetch wal {}'.format(file_id),
                                     'Fetched wal {}'.format(file_id))
+
+    def ping(self):
+        return 'pong'
+
 
 if __name__ == '__main__':
     _ = WaleWrapper()
