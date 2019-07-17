b908127db692d1905f12d7c55d89978a99202b79
Feike Steenbergen <feike@timescale.com>
2019-07-17 10:27:59 +0200

    Skip first backup if backup is not configured
    For some of our offerings we will not be configuring a backup.
    Therefore, we should skip triggering the first backup after
    initialization, otherwise Patroni will not mark the node as being the
    healthiest.

--- scripts/post_init.sh
+++ scripts/post_init.sh
@@ -1,4 +1,11 @@
 #!/bin/bash
+
+if [ ! -f "${PGBACKREST_CONFIG}" ]
+then
+    echo "$(date '+%Y-%m-%d %H:%M:%S') - pgBackRest configuration (${PGBACKREST_CONFIG}) is missing, skipping creation of first backup"
+    exit 0
+fi
+
 echo "$(date '+%Y-%m-%d %H:%M:%S') - creating pgBackRest stanza"
 pgbackrest stanza-create --stanza=poddb --log-level-stderr=info
 
@@ -6,5 +13,6 @@ echo "$(date '+%Y-%m-%d %H:%M:%S') - Waiting for pgBackRest API to become respon
 while ! timeout 1 bash -c "echo > /dev/tcp/localhost/8081"; do
   sleep 1
 done
+
 echo "$(date '+%Y-%m-%d %H:%M:%S') - triggering backup"
 curl -i -X POST http://localhost:8081/backups
