f0eaf8afac578a9e1857ba497b743814b4fb0021
Feike Steenbergen <feike@timescale.com>
2019-07-24 16:44:13 +0200

    Use a separate etcd cluster for failover
    Previously, all deployed instances were using the native kubernetes
    support of Patroni to provide global consensus.
    From now on, new clusters will be using the etcd deployed in the cluster
    specifically for failover.
    
    This ensures that any issues with the k8s etcd does not impact failover
    and vice versa.
    
    Yet again, we need to explicitly inject something in the Patroni
    configuration as the postgres-operator is lacking the path to set the
    namespace used in etcd.
    
    Without this namespace injection, we could have collisions if timescale deployments
    share a name but not a kubernetes namespace.
    
    Does make me wonder if we should get rid of the configure_spilo.py
    script and do it all ourselves. But that is for another day.

--- scripts/augment_patroni_configuration.py
+++ scripts/augment_patroni_configuration.py
@@ -6,30 +6,38 @@ This is a hack to get around Issue: https://github.com/zalando/postgres-operator
 
 
 import yaml
+import os
 import sys
 
 
-def inject_pgbackrest_configuration(filename):
-    with open(filename, 'r+') as f:
-        pgconfig = yaml.safe_load(f)
-        pgconfig['postgresql']['create_replica_methods'] = ['pgbackrest', 'basebackup']
-        pgconfig['postgresql']['pgbackrest'] = {
-            'command': '/usr/bin/pgbackrest --stanza=poddb --delta restore --log-level-stderr=info',
-            'keep_data': True,
-            'no_params': True
-        }
+def inject_pgbackrest_configuration(pgconfig):
+    pgconfig['postgresql']['create_replica_methods'] = ['pgbackrest', 'basebackup']
+    pgconfig['postgresql']['pgbackrest'] = {
+        'command': '/usr/bin/pgbackrest --stanza=poddb --delta restore --log-level-stderr=info',
+        'keep_data': True,
+        'no_params': True
+    }
 
-        pgconfig['bootstrap']['dcs'].setdefault('postgresql', {})
-        pgconfig['bootstrap']['dcs']['postgresql']['recovery_conf'] = {
-            'recovery_target_timeline': 'latest',
-            'standby_mode': 'on',
-            'restore_command': 'pgbackrest --stanza=demo archive-get %f "%p"',
-        }
+    pgconfig['bootstrap']['dcs'].setdefault('postgresql', {})
+    pgconfig['bootstrap']['dcs']['postgresql']['recovery_conf'] = {
+        'recovery_target_timeline': 'latest',
+        'standby_mode': 'on',
+        'restore_command': 'pgbackrest --stanza=demo archive-get %f "%p"',
+    }
+
+
+if __name__ == '__main__':
+    with open(sys.argv[1], 'r+') as f:
+        pgconfig = yaml.safe_load(f)
+        inject_pgbackrest_configuration(pgconfig)
+
+        # This namespace used in etcd/consul
+        # Other provisions are also available, but this ensures no naming collisions
+        # for deployments in separate Kubernetes Namespaces will occur
+        # https://github.com/zalando/patroni/blob/master/docs/ENVIRONMENT.rst#globaluniversal
+        if 'etcd' in pgconfig:
+            pgconfig['namespace'] = os.getenv('POD_NAMESPACE')
 
         f.seek(0)
         yaml.dump(pgconfig, f, default_flow_style=False)
         f.truncate()
-
-
-if __name__ == '__main__':
-    inject_pgbackrest_configuration(sys.argv[1])
