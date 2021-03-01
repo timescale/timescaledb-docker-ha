9433f428d3f4c781c86cfd08d3a675d36abd6435
Joshua Lockerman <josh@timescale.com>
2021-03-01 18:13:13 -0500

    Add permissions to experimental schema

---
+++ scripts/timescale_analytics/after-create.sql
@@ -0,0 +1,5 @@
+
+GRANT EXECUTE ON ALL FUNCTIONS IN SCHEMA timescale_analytics_experimental TO PUBLIC;
+GRANT EXECUTE ON ALL PROCEDURES IN SCHEMA timescale_analytics_experimental TO PUBLIC;
+GRANT EXECUTE ON ALL ROUTINES IN SCHEMA timescale_analytics_experimental TO PUBLIC;
+
