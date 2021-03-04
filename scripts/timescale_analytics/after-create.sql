db85861277d269ce2d0a835762eb6ad86f3b7bc2
Joshua Lockerman <josh@timescale.com>
2021-03-04 11:43:12 -0500

    change access to db owner

--- scripts/timescale_analytics/after-create.sql
+++ scripts/timescale_analytics/after-create.sql
@@ -1,5 +1,5 @@
 
-GRANT USAGE ON SCHEMA timescale_analytics_experimental TO public;
-GRANT EXECUTE ON ALL FUNCTIONS IN SCHEMA timescale_analytics_experimental TO PUBLIC;
-GRANT EXECUTE ON ALL PROCEDURES IN SCHEMA timescale_analytics_experimental TO PUBLIC;
-GRANT EXECUTE ON ALL ROUTINES IN SCHEMA timescale_analytics_experimental TO PUBLIC;
+GRANT USAGE ON SCHEMA timescale_analytics_experimental TO @database_owner@;
+GRANT EXECUTE ON ALL FUNCTIONS IN SCHEMA timescale_analytics_experimental TO @database_owner@;
+GRANT EXECUTE ON ALL PROCEDURES IN SCHEMA timescale_analytics_experimental TO @database_owner@;
+GRANT EXECUTE ON ALL ROUTINES IN SCHEMA timescale_analytics_experimental TO @database_owner@;
