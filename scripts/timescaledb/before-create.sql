b54deba33426ad1ce42f20ff8456f3d40b3a6840
Oleksii Kliukin <oleksii@timescale.com>
2022-01-28 15:34:35 +0100

    Make sure before create check is only triggered when the extension is missing.

--- scripts/timescaledb/before-create.sql
+++ scripts/timescaledb/before-create.sql
@@ -3,18 +3,19 @@
 DO $$
 BEGIN
     IF EXISTS(
-    SELECT
-        1
-    FROM
-        pg_catalog.pg_namespace
-            WHERE nspname IN (
-                '_timescaledb_catalog',
-                '_timescaledb_internal'
-                '_timescaledb_cache',
-                '_timescaledb_config',
-                'timescaledb_experimental',
-                'timescaledb_information'
-            )
+        SELECT
+            1
+        FROM
+            pg_catalog.pg_namespace
+        WHERE nspname IN (
+            '_timescaledb_catalog',
+            '_timescaledb_internal'
+            '_timescaledb_cache',
+            '_timescaledb_config',
+            'timescaledb_experimental',
+            'timescaledb_information'
+        )
+        AND NOT EXISTS(SELECT 1 FROM pg_catalog.pg_extension WHERE extname = 'timescaledb')
     )
     THEN
         RAISE EXCEPTION 'Internal timescaledb schemas are present in the database, but timescaledb extension is missing'
