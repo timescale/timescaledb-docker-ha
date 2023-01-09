19f7f019126a0994f46ba2f8244b59c594489a6e
Brent Graveland <brent@timescale.com>
2023-01-09 07:18:28 -0700

    Add a table into the GH step summary

--- cicd/shared.sh
+++ cicd/shared.sh
@@ -39,6 +39,10 @@ error() {
     EXIT_STATUS=1
 }
 
+available_pg_versions() {
+    (cd /usr/lib/postgresql && ls)
+}
+
 check_base_age() {
     # only check the BUILD_DATE during CI
     if [ "$CI" != true ]; then return 0; fi
@@ -66,14 +70,18 @@ check_base_components() {
 
 check_timescaledb() {
     local pg="$1" lib="$2" unsupported_reason found=false
+
     if [ -z "$TIMESCALEDB_VERSIONS" ]; then
         error "no timescaledb versions requested, why are we here?"
         return 1
     fi
 
+    # record an empty version so we'll get an empty table row if we don't have any versions
+    record_ext_version timescaledb "$pg" ""
+
     for ver in $TIMESCALEDB_VERSIONS; do
         if [[ "$ver" = master || "$ver" = main ]]; then
-            log "skipping looking for timescaledb-$ver"
+            #log "skipping looking for timescaledb-$ver"
             continue
         fi
         if [[ -s "$lib/timescaledb-$ver.so" ]]; then
@@ -81,13 +89,15 @@ check_timescaledb() {
                 if [ -s "$lib/timescaledb-tsl-$ver.so" ]; then
                     error "found timescaledb-tsl-$ver for pg$pg"
                 else
-                    log "found timescaledb-$ver for pg$pg"
+                    #log "found timescaledb-$ver for pg$pg"
                     found=true
+                    record_ext_version timescaledb "$pg" "$ver"
                 fi
             else
                 if [ -s "$lib/timescaledb-tsl-$ver.so" ]; then
-                    log "found timescaledb-$ver and tsl-$ver for pg$pg"
+                    #log "found timescaledb-$ver and tsl-$ver for pg$pg"
                     found=true
+                    record_ext_version timescaledb "$pg" "$ver"
                 else
                     error "found timescaledb-$ver, but not tsl-$ver for pg$pg"
                 fi
@@ -124,14 +134,18 @@ check_promscale() {
         return
     fi
 
+    # record an empty version so we'll get an empty table row if we don't have any versions
+    record_ext_version promscale_extension "$pg" ""
+
     for ver in $PROMSCALE_VERSIONS; do
         if [[ "$ver" = master || "$ver" = main ]]; then
             log "skipping looking for promscale-$ver"
             continue
         fi
         if [ -s "$lib/promscale-$ver.so" ]; then
-            log "found promscale-$ver for pg$pg"
+            #log "found promscale-$ver for pg$pg"
             found=true
+            record_ext_version promscale_extension "$pg" "$ver"
         else
             unsupported_reason="$(supported_promscale "$pg" "$ver")"
             if [ -n "$unsupported_reason" ]; then
@@ -154,6 +168,9 @@ check_toolkit() {
         return
     fi
 
+    # record an empty version so we'll get an empty table row if we don't have any versions
+    record_ext_version toolkit "$pg" ""
+
     for ver in $TOOLKIT_VERSIONS; do
         if [[ "$ver" = master || "$ver" = main ]]; then
             log "skipping looking for toolkit-$ver"
@@ -161,8 +178,9 @@ check_toolkit() {
         fi
 
         if [ -s "$lib/timescaledb_toolkit-$ver.so" ]; then
-            log "found toolkit-$ver for pg$pg"
+            #log "found toolkit-$ver for pg$pg"
             found=true
+            record_ext_version toolkit "$pg" "$ver"
         else
             unsupported_reason="$(supported_toolkit "$pg" "$ver")"
             if [ -n "$unsupported_reason" ]; then
@@ -180,35 +198,43 @@ check_toolkit() {
 check_others() {
     local pg="$1" lib="$2" version status
 
+    record_ext_version logerrors "$pg" ""
     if [ -n "$PG_LOGERRORS" ]; then
         if [ -s "$lib/logerrors.so" ]; then
-            log "found logerrors for pg$pg"
+            #log "found logerrors for pg$pg"
+            record_ext_version logerrors "$pg" "$PG_LOGERRORS"
         else
             error "logerrors not found for pg$pg"
         fi
     fi
 
+    record_ext_version pg_stat_monitor "$pg" ""
     if [ -n "$PG_STAT_MONITOR" ]; then
         if [ -s "$lib/pg_stat_monitor.so" ]; then
-            log "found pg_stat_monitor for pg$pg"
+            #log "found pg_stat_monitor for pg$pg"
+            record_ext_version pg_stat_monitor "$pg" "$PG_STAT_MONITOR"
         else
             error "pg_stat_monitor not found for pg$pg"
         fi
     fi
 
+    record_ext_version pg_auth_mon "$pg" ""
     if [ -n "$PG_AUTH_MON" ]; then
         if [ -s "$lib/pg_auth_mon.so" ]; then
-            log "found pg_auth_mon for pg$pg"
+            #log "found pg_auth_mon for pg$pg"
+            record_ext_version pg_auth_mon "$pg" "$PG_AUTH_MON"
         else
             error "pg_auth_mon not found for pg$pg"
         fi
     fi
 
+    record_ext_version postgis "$pg" ""
     if [ -n "$POSTGIS_VERSIONS" ]; then
         for ver in $POSTGIS_VERSIONS; do
             IFS=\| read -rs version status <<< "$(dpkg-query -W -f '${version}|${status}' "postgresql-$pg-postgis-$ver")"
             if [ "$status" = "install ok installed" ]; then
-                log "found pg$pg extension postgis-$version"
+                #log "found pg$pg extension postgis-$version"
+                record_ext_version postgis "$pg" "$version"
             else
                 error "pg$pg extension postgis-$ver not found: $status"
             fi
@@ -216,9 +242,11 @@ check_others() {
     fi
 
     for extname in $PG_WANTED_EXTENSIONS; do
+        record_ext_version "$extname" "$pg" ""
         IFS=\| read -rs version status <<< "$(dpkg-query -W -f '${version}|${status}' "postgresql-$pg-$extname")"
         if [ "$status" = "install ok installed" ]; then
-            log "found pg$pg extension $extname-$version"
+            #log "found pg$pg extension $extname-$version"
+            record_ext_version $extname "$pg" "$version"
         else
             error "pg$pg extension $extname not found: $status"
         fi
@@ -234,5 +262,37 @@ check_packages() {
         else
             error "package $pkg not found: $status"
         fi
+    done
+}
+
+record_ext_version() {
+    local pkg="$1" pg="$2" version="$3"
+    echo "$pkg|$version" >> "/tmp/extversions.$ARCH.pg$pg"
+}
+
+sort_keys() {
+    for k in "$@"; do echo "$k"; done | xargs | sort -ifVu
+}
+
+ext_version_table() {
+    local -A pkgs
+    local versions
+    echo "#### Installed PG extensions for $ARCH:"
+    for pg in $(available_pg_versions); do
+        echo "| PG$pg Extension | Versions |"
+        echo "|:-|:-|"
+
+        pkgs=()
+        while read -r line; do
+            IFS=\| read -rs pkg version <<< "$line"
+            pkgs["$pkg"]+=" $version"
+        done < "/tmp/extversions.$ARCH.pg$pg"
+
+        for pkg in $(sort_keys "${!pkgs[@]}"); do
+            versions="$(sort_keys "${pkgs[$pkg]}")"
+            echo "| $pkg | ${versions// /, } |"
+        done
+
+        echo
     done
 }
