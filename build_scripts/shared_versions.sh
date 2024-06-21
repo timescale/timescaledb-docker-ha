4256559e9d2715c30ed04bc03850b9c150d51120
Matvey Arye <cevian@gmail.com>
2024-06-21 10:31:51 -0400

    Add arm for pgvectorscale

--- build_scripts/shared_versions.sh
+++ build_scripts/shared_versions.sh
@@ -182,8 +182,6 @@ supported_promscale() {
 supported_pgvectorscale() {
     local pg="$1" ver="$2"
 
-    if [ "$ARCH" != amd64 ]; then echo "unsupported arch $ARCH"; return; fi
-
     # just attempt the build for main/master/or other branch build
     if [[ "$ver" = main || "$ver" = master || "$ver" =~ [a-z_-]*/[A-Za-z0-9_-]* ]]; then
         return
