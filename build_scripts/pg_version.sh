5d636fe81c6ed4dec28607da357bad84b9243588
ndn <niconosenzo@gmail.com>
2025-12-03 18:38:09 +0100

    fix: use major.minor version for deb package suffix
    Make use of the major.minor versions pinned in versions.yaml file for
    the deb package suffix.

--- build_scripts/pg_version.sh
+++ build_scripts/pg_version.sh
@@ -1,12 +1,14 @@
 #!/bin/bash
 
+SCRIPT_DIR="${BASH_SOURCE[0]%/*}"
+
 MAJOR="${1}"
 if [ -z "${MAJOR}" ]; then
 	echo "missing major version"
 	exit 2
 fi
 
-PINNED=$(yq ".postgres_versions.${MAJOR}" /build/scripts/versions.yaml)
+PINNED=$(yq ".postgres_versions[\"${MAJOR}\"]" "$SCRIPT_DIR/versions.yaml")
 if [ "${PINNED}" = "null" ]; then
 	echo "could not find ${MAJOR} pinned version"
 	exit 2
